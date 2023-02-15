fd689386b6bf64e18345f1e1767f076c643f20a4
Lucas Gimenes <lucas_sin5@hotmail.com>
2023-02-15 11:45:44 -0300

    add: molecules/MainBanner

---
+++ src/atomic/molecules/MainBanner.tsx
@@ -0,0 +1,20 @@
+import React from 'react';
+import { Box, Image } from 'native-base';
+import { ImageBackground } from 'react-native';
+
+import slider from '../../img/slider.png';
+import bike from '../../img/bike.png';
+
+export function MainBanner() {
+  return (
+    <Box w="100%" h="300" mt="60px">
+      <ImageBackground
+        source={slider}
+        resizeMode="contain"
+        style={{ flex: 1, justifyContent: "center", alignItems: "center" }}
+      >
+        <Image source={bike} alt="image of a bike"/>
+      </ImageBackground>
+    </Box>
+  );
+}
